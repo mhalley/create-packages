8ef7b1bbf3b089b0b53e95f2b25d1cf38fbcf0e4
Marguerite Halley <marguerite.e.halley@gmail.com>
2024-07-23 20:34:56 +0200

    Upload completed files to create a package
    Included are the python init file and three python files that include functions related to different mathematical equations.

---
+++ geometry.py
@@ -0,0 +1,15 @@
+# Import the 'pi' constant from the 'math' module
+from math import pi
+
+def area_of_rectangle(a, b):
+    """
+    This function multiplies 2 numbers to return the area of a rectangle
+    """
+    return a * b
+
+def area_of_circle(a):
+    """
+    This function return the area of a circle
+    """
+    return  pi * a ** 2
+
